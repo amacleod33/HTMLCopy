4ae3d906995cb448ae1d2e83f11baef67c2cc418
Alexander MacLeod <alexander.macleod.18@cnu.edu>
2020-09-16 18:19:37 -0400

    Refactored code. Created javadoc.

--- test/src/main/java/test/HTMLStrings.java
+++ test/src/main/java/test/HTMLStrings.java
@@ -1,6 +1,7 @@
 package test;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * HTML Copy editor PSP0
@@ -11,28 +12,21 @@ import java.util.ArrayList;
 public class HTMLStrings {
 
     /**
-     * Copy editor
+     * Gets tags that are opened before beginning of indexes
      * 
-     * @param input
-     *            csv of html code to copy
-     * @return //
+     * @param begin
+     *            Beginning index
+     * @param end
+     *            End index
+     * @param stringArr
+     *            List of characters from input array
+     * @return Array containing all tags before starting index
      */
-    public static String copy(String input) {
+    private static
+        List<String>
+        getOpenTags(int begin, int end, String[] stringArr) {
         String copy = "";
-        String[] stringToArr = input.split(",");
-        int begin = Integer.parseInt(stringToArr[0]);
-        int end = Integer.parseInt(stringToArr[1]);
-
-        // if input is null
-        if (begin == 0 && end == 0) {
-            return "";
-        }
-
-        String[] stringArr = stringToArr[2].split("");
         ArrayList<String> messageArr = new ArrayList<String>();
-        ArrayList<String> tagArr = new ArrayList<String>();
-
-        // Finds start tags
         for (int i = 0; i < begin; i++) {
             if (stringArr[i].equals("<")) {
                 if (stringArr[i + 1].equals("/")) {
@@ -52,8 +46,31 @@ public class HTMLStrings {
             }
 
         }
+        return messageArr;
+    }
 
-        // Finds message, including tags if it's in the message
+
+    /**
+     * Gets the message between start and end indexes, checks to see if tags are
+     * already added to messageArr or not
+     * 
+     * @param begin
+     *            Beginning index
+     * @param end
+     *            Ending index
+     * @param messageArr
+     *            Array of beginning tags
+     * @param stringArr
+     *            Array of string from input
+     * @return Modified array of all beginning tags and end and message between
+     *         indexes
+     */
+    private static List<String> getMessageBetweenTags(
+        int begin,
+        int end,
+        List<String> messageArr,
+        String[] stringArr) {
+        String copy = "";
         for (int i = begin; i < end; i++) {
 
             // finds tags
@@ -72,12 +89,30 @@ public class HTMLStrings {
 
             }
         }
+        return messageArr;
+    }
 
-        copy = "";
 
-        // Finds end tags
-
-        // Guard if index starts at zero so loop will be applied
+    /**
+     * Finds the end tags of all tags that were already opened.
+     * 
+     * @param begin
+     *            Beginning index
+     * @param end
+     *            End index
+     * @param stringArr
+     *            Array of all characters from input
+     * @param messageArr
+     *            Array with message and beginning tags
+     * @return Array of tags that should be added to end of message
+     */
+    private static List<String> getEndTags(
+        int begin,
+        int end,
+        String[] stringArr,
+        List<String> messageArr) {
+        List<String> tagArr = new ArrayList<String>();
+        String copy = "";
         if (begin == 0) {
             begin = end;
         }
@@ -111,6 +146,28 @@ public class HTMLStrings {
             }
 
         }
+        return tagArr;
+    }
+
+
+    /**
+     * Determines if tags have been closed before the starting index; if they
+     * have been they're removed
+     * 
+     * @param begin
+     *            Beginning index
+     * @param end
+     *            End index
+     * @param stringArr
+     *            Array of all characters in input
+     * @param messageArr
+     *            Array of all tags and message
+     */
+    private static void findClosedTagsBeforeStart(
+        int begin,
+        int end,
+        String[] stringArr,
+        List<String> messageArr) {
         String temp = "<";
 
         // Guard for if indexes are whole string length
@@ -134,7 +191,31 @@ public class HTMLStrings {
                 }
             }
         }
+    }
 
+
+    /**
+     * Puts together messageArr and tagArr to form final string
+     * 
+     * @param begin
+     *            Beginning index
+     * @param end
+     *            End index
+     * @param length
+     *            Length of input string
+     * @param messageArr
+     *            Array containing beginning tags and message between indexes
+     * @param tagArr
+     *            Array of end tags
+     * @return Final string to be returned to user
+     */
+    private static String getFinalString(
+        int begin,
+        int end,
+        int length,
+        List<String> messageArr,
+        List<String> tagArr) {
+        String copy = "";
         // Iterates through the arrayList to make it one string which can be
         // returned.
         for (String s : messageArr) {
@@ -147,17 +228,60 @@ public class HTMLStrings {
 
             // Does not add the tags at the end if already contained in the
             // message.
-            if (end == stringArr.length) {
+            if (end == length) {
                 break;
             }
             copy += tagArr.get(i);
         }
+
         return copy;
     }
 
 
     /**
-     * Main method
+     * Copy editor
+     * 
+     * @param input
+     *            csv of html code to copy
+     * @return String to be copied from input
+     */
+    public static String copy(String input) {
+        String copy = "";
+        String[] stringToArr = input.split(",");
+        int begin = Integer.parseInt(stringToArr[0]);
+        int end = Integer.parseInt(stringToArr[1]);
+
+        // if input is null
+        if (begin == 0 && end == 0) {
+            return "";
+        }
+
+        String[] stringArr = stringToArr[2].split("");
+        List<String> messageArr = new ArrayList<String>();
+        List<String> tagArr = new ArrayList<String>();
+
+        // Finds open tags
+        messageArr = getOpenTags(begin, end, stringArr);
+
+        // Finds message, including tags if it's in the message
+        messageArr = getMessageBetweenTags(begin, end, messageArr, stringArr);
+
+        // Finds end tags
+        tagArr = getEndTags(begin, end, stringArr, messageArr);
+
+        // Finds if tags are closed before start of index
+        findClosedTagsBeforeStart(begin, end, stringArr, messageArr);
+
+        // Iterates through the arrayList to make it one string which can be
+        // returned.
+        copy = getFinalString(begin, end, stringArr.length, messageArr, tagArr);
+
+        return copy;
+    }
+
+
+    /**
+     * Main method for testing
      * 
      * @param args
      *            arguments
